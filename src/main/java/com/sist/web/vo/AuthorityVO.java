7ade164d966ef80c2e68231d33e3e33c3d4e7e64
ki-sd <redringo4@gmail.com>
2026-08-18 17:24:27 +0900

    feat: SpringSecurity 도입

---
+++ src/main/java/com/sist/web/vo/AuthorityVO.java
@@ -0,0 +1,10 @@
+package com.sist.web.vo;
+
+import lombok.Data;
+
+//USERID             VARCHAR2(20) 
+//AUTHORITY NOT NULL VARCHAR2(20) 
+@Data
+public class AuthorityVO {
+	private String userid,authority;
+}
