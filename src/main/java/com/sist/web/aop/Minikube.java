412d0d2ce89191cbba901f95557946143289cca4
ki-sd <redringo4@gmail.com>
2026-08-21 17:09:38 +0900

    study: 댓글 기능 확장 (REST API 삭제/수정 엔드포인트 구현, Pinia actions 연동 및 소유자 기반 UI 제어)

---
+++ src/main/java/com/sist/web/aop/Minikube.java
@@ -0,0 +1,13 @@
+package com.sist.web.aop;
+/*
+ *     minikube 사용 목적
+ *     1) 비용이 들지 않는다
+ *     2) 자신 컴퓨터에서 쿠버네티스 사용 가능 (테스트)
+ *                   ------- 컨테이너 (도커)
+ *                   ------- DockerHub
+ *     minikube 실행
+ *        
+ */
+public class Minikube {
+
+}
